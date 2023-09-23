0c01f37f1de3d146e1828d95222a7d5a0e9b3ff9
RYiOSDev <1859295+ryiosdev@users.noreply.github.com>
2023-09-23 12:33:22 -0500

    Update FiestaButtonStyle.swift

--- Sources/FiestaUI/FiestaButtonStyle.swift
+++ Sources/FiestaUI/FiestaButtonStyle.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 /// A custom ButtonStyle that applies the FiestaTheme to standard SwiftUI Buttons
 ///
-/// You can either pass in a `FiestaButtonStyle` instance to the `buttonStyle` view modifier or the convenience View extension `.fiestaStyle`
+/// You can either pass in a `FiestaButtonStyle` instance to the `buttonStyle` view modifier or the convenience View extension `.fiestaButtonStyle` which can be used on container views.
+///
 /// You may also override the standard internal padding by passing in the `padding` parameter to the ``init(padding:)`` method which is helpful for buttons with images.
 /// Sample usage:
 ///  ```swift
@@ -23,12 +24,14 @@ import SwiftUI
 ///Button("Cancel", role: .cancel) { }
 ///   .fiestaStyle()
 ///
-///Button("Delete", role: .destructive) { }
-///    .fiestaStyle()
+///Button(action: {}) {
+///    Image("SwiftUI")
+/// }.buttonStyle(FiestaButtonStyle(padding: 0.0))
 ///```
 public struct FiestaButtonStyle: ButtonStyle {
     @Environment(\.theme) private var theme: Theme
     @Environment(\.isEnabled) private var enabled: Bool
+    @Environment(\.colorScheme) private var scheme
 
     /// Optional padding override of the default Fiesta theme (``FiestaTheme.padding``)  value applied to the contents of the button
     private var paddingOverride: CGFloat?
@@ -44,23 +47,24 @@ public struct FiestaButtonStyle: ButtonStyle {
         // Apply Default styling
         var backgroundColor = theme.colorBackgroundButton
         var foregroundColor = theme.colorTextButton
-        var borderColor = theme.colorBorderButton
+        var borderColor = scheme == .light ? theme.colorBorderButton : .fiesta(.white)
         
         // If the button has a role specified, adjust the styling
         if configuration.role == .cancel {
-            backgroundColor = theme.colorBackgroundButtonSecondary
+            backgroundColor = scheme == .light ? theme.colorBackgroundButtonSecondary : .fiesta(.black)
             foregroundColor = theme.colorTextButtonSecondary
             borderColor = theme.colorBorderButtonSecondary
             
         } else if configuration.role == .destructive {
-            backgroundColor = theme.colorBackgroundButtonDestructive
+            backgroundColor = scheme == .light ? theme.colorBackgroundButtonDestructive : .fiesta(.black)
+
             foregroundColor = theme.colorTextButtonDestructive
             borderColor = theme.colorBorderButtonDestructive
         }
         
         // Apply fancy border
-        let borderStroke = configuration.isPressed ? StrokeStyle(lineWidth: 2, dash: [2]) : StrokeStyle()
-        
+        let borderStroke = configuration.isPressed ? theme.strokeStyleButtonPressed : StrokeStyle()
+
         // Change brightness based on enabled/disabled
         let brightness = enabled ? 0.0 : theme.disabledBrightness
         
@@ -75,13 +79,15 @@ public struct FiestaButtonStyle: ButtonStyle {
                     .strokeBorder(style: borderStroke)
                     .foregroundColor(borderColor)
             }
+            .scaleEffect(configuration.isPressed ? theme.scaleButtonPressed : 1.0)
+            .animation(.easeInOut, value: configuration.isPressed)
             .brightness(brightness)
     }
 }
 
-public extension Button {
-    /// A shortcut to `buttonStyle(FiestaButtonStyle())`
-    func fiestaStyle() -> some View {
+public extension View {
+    /// A shortcut to `.buttonStyle(FiestaButtonStyle())`
+    func fiestaButtonStyle() -> some View {
         buttonStyle(FiestaButtonStyle())
     }
 }
@@ -90,18 +96,44 @@ struct FiestaButton_Previews: PreviewProvider {
     static var previews: some View {
         FiestaUI.loadFonts()
         return Group {
-            Button("Basic") { }
-                .buttonStyle(FiestaButtonStyle())
-            
-            Button(action: {}) {
-                Label("Label", systemImage: "party.popper")
-            }.fiestaStyle()
+            HStack {
+                basic
+                basic.disabled(true)
+            }
+            .previewDisplayName("Basic")
 
+            HStack {
+                roles
+                roles.disabled(true)
+            }
+            .previewDisplayName("Roles")
+        }
+        .previewLayout(.sizeThatFits)
+        .fiestaButtonStyle()
+    }
+
+    @ViewBuilder static var basic: some View {
+        VStack {
+            Button("Disabled") {}
+            Button(action: {}) {
+                Label("Disabled", systemImage: "party.popper")
+            }
+            Button(action: {}) {
+                Image(systemName: "party.popper")
+            }
+            Button(action: {}) {
+                VStack {
+                    Text("Text 1")
+                    Text("Text 2")
+                }
+            }
+        }
+    }
+
+    @ViewBuilder static var roles: some View {
+        VStack {
             Button("Cancel", role: .cancel) { }
-                .fiestaStyle()
-            
             Button("Delete", role: .destructive) { }
-                .fiestaStyle()
-        }.previewLayout(.sizeThatFits)
+        }
     }
 }
